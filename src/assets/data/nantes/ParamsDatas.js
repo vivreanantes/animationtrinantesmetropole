2ea04c3fc878e5d1c86a1c83c4a0ea4bb77f1b2a
Renoulin Christian <chris-ren@netcourrier.com>
2020-05-01 16:18:07 +0200

    pour la carte

--- src/assets/data/nantes/ParamsDatas.js
+++ src/assets/data/nantes/ParamsDatas.js
@@ -19,3 +19,132 @@ var _paramsDatas = {
 	'available_language' : "fr,en",
 	'defaultlanguage' : "fr"
 };
+
+var _paramFilterTypePlacesDatas = [{
+		"id": "1",
+		"code": "smco_reemp|modco_decheterie|modco_ecopoint|modco_encombrants_resume|ventevracstruct",
+		"nom": "Tous les lieux"
+	}, {
+		"id": "2",
+		"code": "smco_reemp",
+		"nom": "Réemploi"
+	}, {
+		"id": "3",
+		"code": "modco_decheterie|modco_ecopoint",
+		"nom": "Déchèteries / Ecopoints",
+		"nom_en": "Déchèteries / Ecopoints"
+	}, {
+		"id": "4",
+		"code": "modco_encombrants_resume",
+		"nom": "Encombrants",
+		"nom_en": "Containers"
+	}, {
+		"id": "5",
+		"code": "ventevracstruct",
+		"nom": "Vente vrac",
+		"nom_en": "No packaging shops"
+	}
+];
+var _paramFilterTypeFiches = [{
+		"id": "1",
+		"code": "solutions_entreprises_nantes|initiative_locale|zero_dechet_nantes|consigne_locale|general_tri",
+		"nom": "Tout"
+	}, {
+		"id": "2",
+		"code": "solutions_entreprises_nantes",
+		"nom": "Solutions pour les entreprises"
+	}, {
+		"id": "3",
+		"code": "initiative_locale",
+		"nom": "Initiatives locales réduc. déchets"
+	}, {
+		"id": "4",
+		"code": "zero_dechet_nantes",
+		"nom": "Mode de vie zéro déchets"
+	}, {
+		"id": "5",
+		"code": "consigne_locale|general_tri",
+		"nom": "Collecte des déchets"
+	}
+];
+
+var _paramFilterTypeMapDatas = [{
+		"id": "1",
+		"code": "smco_reemp",
+		"nom": "Réemploi (77)",
+		"nom_en": "Re-use (77)",
+		"image": "resources/icons/marker-icon-blue.png"
+	}, {
+		"id": "2",
+		"code": "modco_contmpb,modco_contverre,modco_contembjournmag,modco_contomr,modco_dechetssecs",
+		"nom": "Conteneurs (1580)",
+		"nom_en": "Containers (1580)",
+		"image": "resources/icons/marker-icon-brown.png"
+	}, {
+		"id": "3",
+		"code": "modco_ecopoint,modco_decheterie",
+		"nom": "Déchèteries / Ecopoints (16)",
+		"nom_en": "Déchèteries / Ecopoints (16)",
+		"image": "resources/icons/marker-icon-green.png"
+	}, {
+		"id": "4",
+		"code": "modco_compostage",
+		"nom": "Composteurs (156)",
+		"nom_en": "Composting (156)",
+		"image": "resources/icons/marker-icon-yellow.png"
+	}, {
+		"id": "5",
+		"code": "smco_conteneurlerelais",
+		"nom": "Conteneurs vêtements (53)",
+		"nom_en": "Containers clothes (53)",
+		"image": "resources/icons/marker-icon-pink.png"
+	}, {
+		"id": "6",
+		"code": "ventevracstruct,ventevracpoints",
+		"nom": "Vente vrac (105)",
+		"nom_en": "No packaging shops (83)",
+		"image": "resources/icons/marker-icon-red.png"
+	}, {
+		"id": "7",
+		"code": "trisacs",
+		"nom": "Trisac (116)",
+		"nom_en": "Trisac (116)",
+		"image": "resources/icons/marker-icon-purple.png"
+	}, {
+		"id": "8",
+		"code": "modco_bouchons",
+		"nom": "Collecteurs bouchons (38)",
+		"nom_en": "Collector corks (38)",
+		"image": "resources/icons/marker-icon-grey.png"
+	}
+];
+
+var paramAllPlaces = 'smco_reemp|modco_decheterie|modco_ecopoint|modco_encombrants_resume|ventevracstruct';
+
+var paramIconsMap = {
+			default_icon: {},
+			reemploi: {
+				iconUrl: "resources/icons/marker-icon-blue.png"
+			},
+			conteneurs: {
+				iconUrl: "resources/icons/marker-icon-brown.png"
+			},
+			decheterie: {
+				iconUrl: "resources/icons/marker-icon-green.png"
+			},
+			composteurs: {
+				iconUrl: "resources/icons/marker-icon-yellow.png"
+			},
+			conteneurlerelais: {
+				iconUrl: "resources/icons/marker-icon-pink.png"
+			},
+			ventevrac: {
+				iconUrl: "resources/icons/marker-icon-red.png"
+			},
+			trisac: {
+				iconUrl: "resources/icons/marker-icon-purple.png"
+			},
+			collectors: {
+				iconUrl: "resources/icons/marker-icon-grey.png"
+			}
+		};
