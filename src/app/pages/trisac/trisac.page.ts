2f9a5535d39c081a148c5e320b5add5b11fae2cc
wafou <ng.noelguillaume@gmail.com>
2020-05-18 18:03:55 +0200

    Modification du rendu de la page trisacs ainsi que sa recherche : Issues #6 #8

--- src/app/pages/trisac/trisac.page.ts
+++ src/app/pages/trisac/trisac.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ModalController } from "@ionic/angular";
-import { DataService } from "../../services/data.service";
-
 import { TrisacDetailsPage } from "../trisac-details/trisac-details.page";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: "app-trisac",
@@ -10,66 +9,21 @@ import { TrisacDetailsPage } from "../trisac-details/trisac-details.page";
   styleUrls: ["./trisac.page.scss"],
 })
 export class TrisacPage implements OnInit {
-  trisacsRaw: Array<any> = [];
+  filtres: Array<any> = [];
   trisacs: Array<any> = [];
   input: string = null;
 
   constructor(
     private modalController: ModalController,
-    private dataService: DataService
-  ) {}
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
-    this.dataService.get("nantes/TrisacsData.js").then((trisacs) => {
-      let trisacFiltrer = [];
-      trisacs._structuresDatas.map((trisac, index) => {
-        if (!trisacFiltrer[trisac.zone]) trisacFiltrer[trisac.zone] = [];
-
-        if (!trisacFiltrer[trisac.zone][trisac.type_lieu])
-          trisacFiltrer[trisac.zone][trisac.type_lieu] = [];
-
-        trisacFiltrer[trisac.zone][trisac.type_lieu].push(
-          this._formatTrisac(trisac)
-        );
-        if (index === trisacs._structuresDatas.length - 1) {
-          let newTrisacFiltrer = [];
-          let trisacFiltrerLabel = Object.keys(trisacFiltrer);
-          trisacFiltrerLabel.sort();
-          trisacFiltrerLabel.map((zone, jindex) => {
-            newTrisacFiltrer[zone] = trisacFiltrer[zone];
-            if (jindex === trisacFiltrerLabel.length - 1) {
-              this.trisacsRaw = newTrisacFiltrer;
-              this.filtre();
-            }
-          });
-        }
-      });
+    this.activatedRoute.data.subscribe((data: { trisacs: any }) => {
+      this.filtres = Object.keys(data.trisacs);
+      this.trisacs = data.trisacs;
+      this.input = null;
     });
-  }
-
-  private _formatTrisac(trisac) {
-    trisac.adresse = "";
-    trisac.adresse += trisac.housenumber ? `${trisac.housenumber} ` : "";
-    trisac.adresse += trisac.street ? `${trisac.street} ` : "";
-    trisac.adresse += trisac.city ? `, ${trisac.city} ` : "";
-    trisac.adresse += trisac.postcode ? `(${trisac.postcode}).` : "";
-
-    if (trisac.heure_debut_1) {
-      trisac.horaires = [];
-      Object.keys(trisac).map((item) => {
-        if (item.indexOf("heure_debut_") > -1) {
-          let index = item.match(/\d/g).join("");
-          trisac.horaires[index] = {
-            heure:
-              trisac[`heure_debut_${index}`] +
-              " - " +
-              trisac[`heure_fin_${index}`],
-            jour: trisac[`jour_${index}`],
-          };
-        }
-      });
-    }
-    return trisac;
   }
 
   async details(item) {
@@ -80,46 +34,5 @@ export class TrisacPage implements OnInit {
       },
     });
     return await modal.present();
-  }
-
-  getFirstItem(item) {
-    let keys = Object.keys(item);
-    return item[keys[0]][0];
-  }
-
-  /**
-   * Changement du filtre
-   */
-  onChangeFiltre(data) {
-    this.filtre(data);
-  }
-
-  /**
-   * Remet à zéro le filtre
-   */
-  cleanFiltre() {
-    this.input = null;
-    this.filtre();
-  }
-
-  /**
-   * Filtre les propositions
-   */
-  filtre(value?: any) {
-    if (value) {
-      let trisacs = [];
-      Object.keys(this.trisacsRaw).map((zone, index) => {
-
-        // TODO : il faudrait zone et lieu_distribution
-        if (zone.toLowerCase().indexOf(value) > -1) {
-          trisacs[zone] = this.trisacsRaw[zone];
-        }
-        if (index === Object.keys(this.trisacsRaw).length - 1) {
-          this.trisacs = trisacs;
-        }
-      });
-    } else {
-      this.trisacs = this.trisacsRaw;
-    }
   }
 }
