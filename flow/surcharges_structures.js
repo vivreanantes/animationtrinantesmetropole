b685daa7223d67d724a2e283e945a0762a09dc76
Renoulin Christian <chris-ren@netcourrier.com>
2020-10-12 13:20:47 +0200

    Surchage noeuds Structures

---
+++ flow/surcharges_structures.js
@@ -0,0 +1,6 @@
+{
+  "_surchargesStructures": [{
+    "code" = "osm_270901433",
+    "images" = "www.mieuxtrieranantes.fr/docs/medias/StructuresReemploi/VisiteSolidariMeubles/VisiteSolidariMeubles_DSC_0499.JPG,www.mieuxtrieranantes.fr/docs/medias/StructuresReemploi/VisiteSolidariMeubles/VisiteSolidariMeubles_DSC_0501.JPG,www.mieuxtrieranantes.fr/docs/medias/StructuresReemploi/VisiteSolidariMeubles/VisiteSolidariMeubles_DSC_0504.JPG,www.mieuxtrieranantes.fr/docs/medias/StructuresReemploi/VisiteSolidariMeubles/VisiteSolidariMeubles_DSC_0507.JPG,www.mieuxtrieranantes.fr/docs/medias/StructuresReemploi/VisiteSolidariMeubles/VisiteSolidariMeubles_DSC_0509.JPG"
+  }]
+}
